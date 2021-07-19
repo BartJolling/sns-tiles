7026937dfd9db64b261e28d95ae0891368882e81
Bart Jolling <BartJolling@users.noreply.github.com>
2021-07-19 15:27:55 +0200

    Added quick POC for alternative SnS tiles
    Proof of concept

---
+++ DownloadTiles.ps1
@@ -0,0 +1,9 @@
+﻿$url = "https://www.worldofsns.com/images/Tile"
+$wc = New-Object System.Net.WebClient
+
+For ($i=20; $i -le 26; $i++) 
+{
+  $img = -join($url, $i, ".png");
+  $local = -join("Tile", $i, ".png");
+  $wc.DownloadFile($img, $local);
+}
